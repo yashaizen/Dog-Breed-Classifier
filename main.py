c241a2977659fa7c9aa2f55b6278c45962d839a1
Yash <yashaizen@gmail.com>
2020-05-19 04:41:54 +0530

    Testing snippet added

--- main.py
+++ main.py
@@ -133,3 +133,39 @@ model_scratch = train(100, loaders_scratch, model_scratch, optimizer_scratch,
                       criterion_scratch, use_cuda, 'model_scratch.pt')
 # load the model that got the best validation accuracy
 model_scratch.load_state_dict(torch.load('model_scratch.pt'))
+
+
+
+
+
+def test(loaders, model, criterion, use_cuda):
+
+    # monitor test loss and accuracy
+    test_loss = 0.
+    correct = 0.
+    total = 0.
+
+    model.eval()
+    for batch_idx, (data, target) in enumerate(loaders['test']):
+        # move to GPU
+        if use_cuda:
+            data, target = data.cuda(), target.cuda()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the loss
+        loss = criterion(output, target)
+        # update average test loss 
+        test_loss = test_loss + ((1 / (batch_idx + 1)) * (loss.data - test_loss))
+        # convert output probabilities to predicted class
+        pred = output.data.max(1, keepdim=True)[1]
+        # compare predictions to true label
+        correct += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())
+        total += data.size(0)
+            
+    print('Test Loss: {:.6f}\n'.format(test_loss))
+
+    print('\nTest Accuracy: %2d%% (%2d/%2d)' % (
+        100. * correct / total, correct, total))
+
+# call test function    
+test(loaders_scratch, model_scratch, criterion_scratch, use_cuda)
